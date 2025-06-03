6814ebc8be1f28888e8a399b39ad00ae8b79b64a
satvik25 <satvikchaudhary@gmail.com>
2025-06-03 21:38:26 +0530

    Delete .config/setup/3-archinstall-encrypt.sh

--- .config/setup/3-archinstall-encrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# 3-archinstall-encrypt.sh - encrypt root partition and open it
-set -euo pipefail
-
-# Automatically confirm formatting while still prompting for the passphrase.
-printf 'YES\n' | cryptsetup luksFormat --pbkdf pbkdf2 --label ROOTFS --key-file /dev/tty /dev/sda2
-cryptsetup open /dev/sda2 cryptroot
