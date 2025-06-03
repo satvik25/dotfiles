ff08ee09e6ab703aca640facc19b961bf151c263
satvik25 <satvikchaudhary@gmail.com>
2025-06-03 22:50:47 +0530

    Update 4-archinstall-format.sh

--- .config/setup/4-archinstall-format.sh
+++ .config/setup/4-archinstall-format.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 
 DISK=/dev/sda
 if [[ "$DISK" =~ nvme[0-9]n[0-9]$ ]]; then
-  PART="${DISK}p1"
+  PART_BOOT="${DISK}p1"
 else
-  PART="${DISK}1"
+  PART_BOOT="${DISK}1"
 fi
 MAPPER_NAME=cryptroot
 LABEL_BOOT=BOOTFS
 LABEL_CONTAINER=LUKS_ROOT
 
 # Format the EFI System Partition (ESP)
-mkfs.fat -F 32 -n "$LABEL_BOOT" "$PART"
+mkfs.fat -F 32 -n "$LABEL_BOOT" "$PART_BOOT"
 
 # Format the decrypted LUKS container as Btrfs
 mkfs.btrfs -L "$LABEL_CONTAINER" /dev/mapper/${MAPPER_NAME}
