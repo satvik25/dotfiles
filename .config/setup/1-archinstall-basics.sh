a64aa59dc4bd83c12a459d54a008881c4c653030
satvik25 <satvikchaudhary@gmail.com>
2025-06-04 15:26:56 +0530

    Update 1-archinstall-basics.sh

--- .config/setup/1-archinstall-basics.sh
+++ .config/setup/1-archinstall-basics.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # 1-setfont-ntp.sh - set console font and enable NTP
 set -euo pipefail
+export PS4='+ ${BASH_SOURCE:-$0}:${LINENO}: '
+set -x
 
 setfont ter-120n
 timedatectl set-ntp true
