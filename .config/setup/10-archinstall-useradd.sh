dbdeee801ee017c31dfc91919fe7c78af3bdd888
satvik25 <satvikchaudhary@gmail.com>
2025-06-13 01:27:17 +0530

    Update 10-archinstall-useradd.sh

--- .config/setup/10-archinstall-useradd.sh
+++ .config/setup/10-archinstall-useradd.sh
@@ -13,6 +13,21 @@ SUDOERS_FILE="/etc/sudoers"
 # Create the user with home directory, wheel group, and specified shell
   useradd -m -G "$GROUPS" -s "$SHELL_PATH" "$USERNAME"
   echo "User '$USERNAME' created and added to groups: $GROUPS."
+
+# Read password securely from terminal
+while true; do
+    read -s -p "Enter new password for $USERNAME: " P1 < /dev/tty; echo
+    read -s -p "Confirm password: " P2 < /dev/tty; echo
+
+    if [[ "$P1" == "$P2" ]]; then
+        break
+    else
+        echo "Passwords do not match. Try again." > /dev/tty
+    fi
+done
+
+# Feed passwords to passwd via /dev/tty-compatible input
+passwd "$USERNAME" < <(printf "%s\n%s\n" "$P1" "$P1")
   
 # Uncomment the sudoers line for wheel group
 if grep -q "^# %wheel ALL=(ALL:ALL) ALL" "$SUDOERS_FILE"; then
