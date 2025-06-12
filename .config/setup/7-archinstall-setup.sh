f22bc973d0597c18341c8212abd1c2ea0d89a96d
satvik25 <satvikchaudhary@gmail.com>
2025-06-13 01:24:33 +0530

    Update 7-archinstall-setup.sh

--- .config/setup/7-archinstall-setup.sh
+++ .config/setup/7-archinstall-setup.sh
@@ -15,8 +15,22 @@ locale-gen
 echo "FONT=ter-120n" > /etc/vconsole.conf
 # Hostname
 echo "arch" > /etc/hostname
+
 # Root Password
-# LATER!
+# Read Password
+while true; do
+    read -s -p "Enter new root password: " ROOT1 < /dev/tty; echo
+    read -s -p "Confirm root password: " ROOT2 < /dev/tty; echo
+
+    if [[ "$ROOT1" == "$ROOT2" ]]; then
+        break
+    else
+        echo "Passwords do not match. Try again." > /dev/tty
+    fi
+done
+# Set Password
+passwd root < <(printf "%s\n%s\n" "$ROOT1" "$ROOT1")
+
 # 32-bit Repos
 sed -i '/^\s*#\s*\[multilib\]/,/^$/{s/^\s*#\s*//}' /etc/pacman.conf
 # Wireless Regulatory Domain
