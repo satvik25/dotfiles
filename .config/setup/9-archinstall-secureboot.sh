abe70c6187cc1a59a65cb7583989dacb73b874de
satvik25 <satvikchaudhary@gmail.com>
2025-06-13 09:54:44 +0530

    Update 9-archinstall-secureboot.sh

--- .config/setup/9-archinstall-secureboot.sh
+++ .config/setup/9-archinstall-secureboot.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
-# 9-secureboot.sh - Secure Boot installer
+# Secure Boot setup
 set -euo pipefail
 export PS4='+ ${BASH_SOURCE:-$0}:${LINENO}: '
 set -x
 
-# 7. Setup sbctl for Secure Boot key management
 sbctl create-keys
 sbctl enroll-keys -m
 
-echo "sbctl keys created and enrolled."
+sbctl sign -s /boot/vmlinuz-linux-zen
+sbctl sign -s /boot/vmlinuz-linux-lts
+sbctl sign -s /efi/EFI/GRUB/grubx64.efi
 
-# 8. Sign kernel and GRUB EFI binary for Secure Boot
-# Sign vmlinuz-linux
-  sbctl sign -s /boot/vmlinuz-linux-zen
-  sbctl sign -s /boot/vmlinuz-linux-lts
-  sbctl sign -s /efi/EFI/GRUB/grubx64.efi
-
-# 9. Create fallback boot path
+# Create UEFI known path for bootloader (esp. on DELL laptops)
 mkdir -p /efi/EFI/BOOT
 cp /efi/EFI/GRUB/grubx64.efi /efi/EFI/BOOT/BOOTX64.EFI
 
-echo "Copied GRUB EFI to fallback BOOTX64.EFI."
-
-echo "GRUB + Secure Boot configuration complete."
+echo -e "\033[32m[SUCCESS]\033[0m Secure Boot set."
