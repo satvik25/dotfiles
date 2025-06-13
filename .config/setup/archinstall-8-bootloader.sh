da195d5e96727c789b2a5cb22715f4469c4bc4b7
satvik25 <satvikchaudhary@gmail.com>
2025-06-13 09:50:53 +0530

    Update and rename 8-archinstall-boot.sh to archinstall-8-bootloader.sh

--- .config/setup/archinstall-8-bootloader.sh
+++ .config/setup/archinstall-8-bootloader.sh
@@ -1,36 +1,28 @@
 #!/usr/bin/env bash
-# 8-archinstall-boot.sh - Boot Setup
+# Bootloader
 set -euo pipefail
 export PS4='+ ${BASH_SOURCE:-$0}:${LINENO}: '
 set -x
 
-# 1. Get the UUID of the root partition (unencrypted)
 root_uuid=$(blkid -s UUID -o value /dev/sda2)
-# 2. Get the UUID of the cryptroot device
 crypt_uuid=$(blkid -s UUID -o value /dev/mapper/cryptroot)
-# 3. Get the resume offset of the swapfile
+# [FIX IT] offset is not getting printed
 offset=$(btrfs inspect-internal map-swapfile -r /swap/swapfile | awk '/offset/ {print $2}')
-# 4. In-place edit of /etc/default/grub to enable cryptodisk and set boot parameters
-# Uncomment or set GRUB_ENABLE_CRYPTODISK
+
+# Set parameters
 sed -i 's/^#\?GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
-# Update GRUB_CMDLINE_LINUX_DEFAULT with encryption, root, resume & performance options
+# Notify GRUB of encryption, resume & performance options
 sed -i 's|^#\?GRUB_CMDLINE_LINUX_DEFAULT=.*|GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=UUID='$root_uuid':cryptroot:allow-discards root=/dev/mapper/cryptroot resume=UUID='$crypt_uuid' resume_offset='$offset' zswap.enabled=1 loglevel=3 quiet"|' /etc/default/grub
-# Ensure GRUB_CMDLINE_LINUX is defined (empty if not present)
-grep -q '^GRUB_CMDLINE_LINUX=' /etc/default/grub || echo 'GRUB_CMDLINE_LINUX=""' >> /etc/default/grub
 
-echo "/etc/default/grub updated in-place with encrypted root configuration."
+echo -e "\033[32m[SUCCESS]\033[0m GRUB config updated."
 
-# 5. Install GRUB EFI with TPM module, disable shim lock
+# Install GRUB
 grub-install \
   --target=x86_64-efi \
   --efi-directory=/efi \
   --bootloader-id=GRUB \
   --modules="tpm" \
   --disable-shim-lock
-
-echo "GRUB EFI installed with TPM support."
-
-# 6. Generate GRUB config
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Generated /boot/grub/grub.cfg."
+echo -e "\033[32m[SUCCESS]\033[0m GRUB installed."
