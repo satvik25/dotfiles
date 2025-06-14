59e577a60a9b0e5f830936eadaa79e36962c2fa3
satvik25 <satvikchaudhary@gmail.com>
2025-06-14 16:06:07 +0530

    Update archinstall-3.4-dotfiles.sh

--- .config/setup/archinstall-3.4-dotfiles.sh
+++ .config/setup/archinstall-3.4-dotfiles.sh
@@ -6,10 +6,12 @@ git clone --bare https://github.com/satvik25/dotfiles.git $HOME/.dotfiles
 
 # Create alias
 alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-echo "alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
 
 # Checkout to home directory
 dots checkout
 
 # Curb noise
 dots config --local status.showUntrackedFiles no
+
+# Persist alias
+echo "alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
