13938ac0c6efb3c8a55e322cd2b435a3eb2bd9c0
satvik25 <satvikchaudhary@gmail.com>
2025-06-14 23:05:46 +0530

    Update archinstall-1.4.2-snapshots.sh

--- .config/archinstall/archinstall-1.4.2-snapshots.sh
+++ .config/archinstall/archinstall-1.4.2-snapshots.sh
@@ -14,15 +14,21 @@ else
 fi
 MAPPER_NAME="cryptroot"
 
+SNAPSHOT_OPTS="noatime,compress=zstd"
+
+# Mount root and create snapshots subvolume
 sudo mkdir /mnt
-sudo mount -o subvolid=5 /dev/mapper/cryptroot /mnt
+sudo mount -o subvolid=5 "/dev/mapper/{$MAPPER_NAME}" /mnt
 sudo btrfs subvolume create /mnt/@snapshots
 sudo umount /mnt
+
+# Mount snapshots subvolume
 sudo mkdir -p /.snapshots
-sudo mount -o noatime,ssd,compress=zstd,subvol=@snapshots \
-     /dev/mapper/cryptroot /.snapshots
-     
+sudo mount -o "${SNAPSHOT_OPTS},subvol=@snapshots" "/dev/mapper/{$MAPPER_NAME}" /.snapshots
+
+# Add it to filesystem table     
 echo '/dev/mapper/cryptroot /.snapshots btrfs noatime,ssd,compress=zstd,subvol=@snapshots 0 0' \
   | sudo tee -a /etc/fstab
 
+# Set up snapper
 sudo snapper -c root create-config /
