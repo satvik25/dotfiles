18ce3870431cd0ae6f1e6ec05df90fa58d073efe
satvik25 <satvikchaudhary@gmail.com>
2025-06-14 17:40:38 +0530

    Create archinstall-3.5-autostart.sh

---
+++ .config/setup/archinstall-3.5-autostart.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Enable user units
+
+units=(
+  hyprpolkitagent.service
+  hypridle.service
+  hyprsunset.service
+  waybar.service
+  pipewire.socket
+  p11-kit-server.socket
+  wireplumber.service
+  pipewire-pulse.socket
+  mpd.socket
+)
+
+for u in "${units[@]}"; do
+  if systemctl --user list-unit-files | grep -q "^$u"; then
+    echo "Enabling $u ..."
+    systemctl --user enable --now "$u"
+  else
+    echo "Warning: $u not found; skipping." >&2
+  fi
+done
+
+echo -e "\033[32m[SUCCESS]\033[0m Services enabled."
