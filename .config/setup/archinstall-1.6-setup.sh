85186b2f96509f403b77d16e5840697b985ea030
satvik25 <satvikchaudhary@gmail.com>
2025-06-14 10:39:31 +0530

    Update and rename archinstall-7-setup.sh to archinstall-1.6-setup.sh

--- .config/setup/archinstall-1.6-setup.sh
+++ .config/setup/archinstall-1.6-setup.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
-# Basic system setup
 set -euo pipefail
 export PS4='+ ${BASH_SOURCE:-$0}:${LINENO}: '
 set -x
+
+# Basic system setup
 
 # Time
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 timedatectl set-ntp true
+
 # Language
 echo "LANG=en_GB.UTF-8" > /etc/locale.conf
 sed -i '/^#\s*en_GB.UTF-8 UTF-8/s/^#//' /etc/locale.gen
 locale-gen
+
 # Font
 echo "FONT=ter-120n" > /etc/vconsole.conf
+
 # Hostname
 echo "arch" > /etc/hostname
 
 # Root Password
-# Read Password
 while true; do
     read -s -p "Enter new root password: " P1 < /dev/tty; echo
     read -s -p "Confirm root password: " P2 < /dev/tty; echo
@@ -29,13 +32,15 @@ while true; do
         echo "Passwords do not match. Try again." > /dev/tty
     fi
 done
-# Set Password
+
 passwd root < <(printf "%s\n%s\n" "$P1" "$P1")
 
 # 32-bit Repos
 sed -i '/^\s*#\s*\[multilib\]/,/^$/{s/^\s*#\s*//}' /etc/pacman.conf
+
 # Wireless Regulatory Domain
 sed -i 's/^#\?\s*WIRELESS_REGDOM=.*/WIRELESS_REGDOM="IN"/' /etc/conf.d/wireless-regdom
+
 # Regenerate initramfs
 mkinitcpio -P
 
