6ebc831d2b7b98a83518f4606dccff720ebb5c0d
satvik25 <satvikchaudhary@gmail.com>
2025-06-14 18:38:30 +0530

    Update archinstall-3.4-dotfiles.sh

--- .config/setup/archinstall-3.4-dotfiles.sh
+++ .config/setup/archinstall-3.4-dotfiles.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Set parameters
+GIT_USERNAME="?"
+
 # Check for pre-existing dotfiles directory
 if [[ -d "$HOME/.dotfiles" ]]; then
   read -rp "$HOME/.dotfiles already exists. Delete it? [y/N] " answer < /dev/tty
@@ -11,7 +14,7 @@ if [[ -d "$HOME/.dotfiles" ]]; then
 fi
 
 # Clone dotfiles repo
-git clone --bare https://github.com/satvik25/dotfiles.git $HOME/.dotfiles
+git clone --bare git@github.com:"$GIT_USERNAME"/dotfiles.git $HOME/.dotfiles
 
 # Create alias
 dots() { 
@@ -28,3 +31,17 @@ echo -e "\033[32m[SUCCESS]\033[0m Dotfiles restored."
 
 # Update shell
 exec zsh
+
+# SSH authentication
+# ssh-keygen -t ed25519 -C "your-email@example.com"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_ed25519
+# ssh-add -l
+# Paste output of cat ~/.ssh/id_ed25519.pub to GitHub ▸ Settings ▸ SSH and GPG keys ▸ New SSH key → paste → Add key.
+# ssh -T git@github.com
+
+# Usage
+# Set remote with dots config --global push.autoSetupRemote true
+# Update tracked files with dots add -u or Add new files to track with dots add <xyz>
+# dots commit -m "VERSION_NAME"
+# dots push
