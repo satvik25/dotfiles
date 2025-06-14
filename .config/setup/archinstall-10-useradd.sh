b34e0923bda1bdda43868083871c63f1f99e8631
satvik25 <satvikchaudhary@gmail.com>
2025-06-14 09:57:48 +0530

    Update archinstall-10-useradd.sh

--- .config/setup/archinstall-10-useradd.sh
+++ .config/setup/archinstall-10-useradd.sh
@@ -9,7 +9,7 @@ SUDOERS_FILE="/etc/sudoers"
 
 # Create user(s)
 # [FIX IT] 7vik not added to wheel group
-useradd -m -G wheel -s "$SHELL_PATH" "$USERNAME"
+useradd -m -G "$GROUPS" -s "$SHELL_PATH" "$USERNAME"
 
 # Set user(s) password
 while true; do
