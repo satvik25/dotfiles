24f5d2e45663844c958da0e77c7778e921e0621d
satvik25 <satvikchaudhary@gmail.com>
2025-06-14 10:42:57 +0530

    Update and rename archinstall-8-bootloader.sh to archinstall-1.7-bootloader.sh

--- .config/setup/archinstall-1.7-bootloader.sh
+++ .config/setup/archinstall-1.7-bootloader.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
-# Bootloader setup
 set -euo pipefail
 export PS4='+ ${BASH_SOURCE:-$0}:${LINENO}: '
 set -x
 
+# Bootloader setup
+
+# Set parameters
 root_uuid=$(blkid -s UUID -o value /dev/sda2)
 crypt_uuid=$(blkid -s UUID -o value /dev/mapper/cryptroot)
 # [FIX ATTEMPTED] Check if offset is getting printed. Rewrite this line if not.
 offset=$(btrfs inspect-internal map-swapfile -r /swap/swapfile)
 
-# Set parameters
+# Update GRUB options
 sed -i 's/^#\?GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
-# Notify GRUB of encryption, resume & performance options
-sed -i 's|^#\?GRUB_CMDLINE_LINUX_DEFAULT=.*|GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=UUID='$root_uuid':cryptroot:allow-discards root=/dev/mapper/cryptroot resume=UUID='$crypt_uuid' resume_offset='$offset' zswap.enabled=1 loglevel=3 quiet"|' /etc/default/grub
 
+sed -i \
+  's|^#\?GRUB_CMDLINE_LINUX_DEFAULT=.*|\
+GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=UUID='"$root_uuid"':cryptroot:allow-discards \
+root=/dev/mapper/cryptroot \
+resume=UUID='"$crypt_uuid"' \
+resume_offset='"$offset"' \
+zswap.enabled=1 \
+loglevel=3 \
+quiet"|' \
+  /etc/default/grub
+  
 echo -e "\033[32m[SUCCESS]\033[0m GRUB config updated."
 
 # Install GRUB
@@ -23,6 +34,8 @@ grub-install \
   --bootloader-id=GRUB \
   --modules="tpm" \
   --disable-shim-lock
+
+# Generate GRUB config
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e "\033[32m[SUCCESS]\033[0m GRUB installed."
