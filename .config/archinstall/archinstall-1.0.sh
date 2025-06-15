baa27afc2053261b866e29d70eb4c03c4cb4461c
satvik25 <satvikchaudhary@gmail.com>
2025-06-15 14:25:23 +0530

    Create archinstall-1.0.sh

---
+++ .config/archinstall/archinstall-1.0.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.01-basics.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.02-partition.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.03-encrypt.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.04-format.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.05-mount.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.06-install.sh" | bash
