5481422e6fe6c168e02aa64739a16dc74e51a0d2
satvik25 <satvikchaudhary@gmail.com>
2025-06-16 09:24:25 +0530

    Update archinstall-1.0.sh

--- .config/archinstall/archinstall-1.0.sh
+++ .config/archinstall/archinstall-1.0.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Pre-chroot
+
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.01-basics.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.02-partition.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.03-encrypt.sh" | bash
