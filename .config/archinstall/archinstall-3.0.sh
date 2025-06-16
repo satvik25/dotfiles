66c83dafd30333f1f24a7065917f31a378d3b77b
satvik25 <satvikchaudhary@gmail.com>
2025-06-16 09:25:53 +0530

    Update archinstall-3.0.sh

--- .config/archinstall/archinstall-3.0.sh
+++ .config/archinstall/archinstall-3.0.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Post-chroot
+# Post boot
 
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-3.01-pacman.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-3.02-security.sh" | bash
