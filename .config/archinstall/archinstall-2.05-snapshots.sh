c39884beb47c92f59de4168d6ae2c58890920019
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 19:40:34 +0530

    Update archinstall-2.05-snapshots.sh

--- .config/archinstall/archinstall-2.05-snapshots.sh
+++ .config/archinstall/archinstall-2.05-snapshots.sh
@@ -8,18 +8,18 @@ set -x
 
 # Set up snapper root config
 ## Delete own .snapshots directory
-sudo umount /.snapshots 2>/dev/null || true
-sudo rm -rf /.snapshots
+umount /.snapshots 2>/dev/null || true
+rm -rf /.snapshots
 ## Create and remove automatic snapper root config
-sudo snapper -c root create-config /
-sudo umount /.snapshots 2>/dev/null || true
-sudo rm -rf /.snapshots
-sudo rm -f /etc/snapper/configs/root
+snapper -c root create-config /
+umount /.snapshots 2>/dev/null || true
+rm -rf /.snapshots
+rm -f /etc/snapper/configs/root
 ## Create own snapper config
-sudo mkdir -p /.snapshots
-sudo mount /.snapshots
-sudo mkdir -p /etc/snapper/configs
-sudo tee /etc/snapper/configs/root > /dev/null <<EOF
+mkdir -p /.snapshots
+mount /.snapshots
+mkdir -p /etc/snapper/configs
+tee /etc/snapper/configs/root > /dev/null <<EOF
 SUBVOLUME="/"
 ALLOW_USERS="root"
 TIMELINE_CREATE="no"
@@ -35,11 +35,11 @@ NUMBER_LIMIT_IMPORTANT="10"
 EOF
 
 # Set @ as default subvolume
-sudo btrfs subvolume set-default "$(btrfs subvolume list / | awk '{if ($(NF) == "@") print $2}')" /
+btrfs subvolume set-default "$(btrfs subvolume list / | awk '{if ($(NF) == "@") print $2}')" /
 
 # Enable GRUB snapshots menu and generate config
-sudo systemctl enable grub-btrfsd.service
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable grub-btrfsd.service
+grub-mkconfig -o /boot/grub/grub.cfg
 
 set +x
 echo -e "\033[32m[SUCCESS]\033[0m Added snapshot capability."
