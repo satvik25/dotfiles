40ee0978c0992451195f55baa0c3c910f18941a7
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 14:21:03 +0530

    Update archinstall-2.06-useradd.sh

--- .config/archinstall/archinstall-2.06-useradd.sh
+++ .config/archinstall/archinstall-2.06-useradd.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
+export PS4='+ ${BASH_SOURCE:-$0}:${LINENO}: '
+set -x
 
 # Add user(s)
 
@@ -12,6 +14,7 @@ USERNAME="7vik"
 useradd -m -G "$GROUP" -s "$SHELL_PATH" "$USERNAME"
 
 # Set user(s) password
+set +x
 while true; do
     read -s -p "Enter new password for $USERNAME: " P1 < /dev/tty; echo
     read -s -p "Confirm password: " P2 < /dev/tty; echo
@@ -26,10 +29,12 @@ done
 passwd "$USERNAME" < <(printf "%s\n%s\n" "$P1" "$P1")
 
 unset P1 P2
+set -x
 
 # Add user(s) to sudo
 if grep -q "^# %wheel ALL=(ALL:ALL) ALL" "$/etc/sudoers"; then
   sed -i 's/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' "$/etc/sudoers"
 fi
 
+set +x
 echo -e "\033[32m[SUCCESS]\033[0m User(s) "$USERNAME" added."
