c6bc284e82510024ad37baad53b28611c23addf4
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 23:44:41 +0530

    Delete .config/archinstall/archinstall-3.09.sh

--- .config/archinstall/archinstall-3.09.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo -e "Boot up and run the \033[31mnext [3.0] batch\033[0m of scripts."
-echo -e "\033[32m[CONGRATULATIONS!]\033[0m \033[34mArch set up.\033[0m"
