7b2ccdee54618719975de8dd6a4a52fb7a6bcce9
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 14:25:21 +0530

    Update archinstall-3.03-dotfiles.sh

--- .config/archinstall/archinstall-3.03-dotfiles.sh
+++ .config/archinstall/archinstall-3.03-dotfiles.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Usage
+# Set remote with dots config --global push.autoSetupRemote true.
+# Update tracked files with dots add -u or Add new files to track with dots add <xyz>.
+# dots commit -m "VERSION_NAME"
+# dots push
+# Update local dotfiles with dots pull --rebase origin master and then dots push if newer changes made to repo from another machine.
+
+# SSH authentication
+# ssh-keygen -t ed25519 -C "your-email@example.com"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_ed25519
+# ssh-add -l
+# Paste output of cat ~/.ssh/id_ed25519.pub to GitHub ▸ Settings ▸ SSH and GPG keys ▸ New SSH key → paste → Add key.
+# ssh -T git@github.com
+
+export PS4='+ ${BASH_SOURCE:-$0}:${LINENO}: '
+set -x
+
 # Set parameters
-GIT_USERNAME="?"
+GIT_USERNAME="satvik25"
 
 # Check for pre-existing dotfiles directory
 if [[ -d "$HOME/.dotfiles" ]]; then
@@ -31,18 +49,3 @@ echo -e "\033[32m[SUCCESS]\033[0m Dotfiles restored."
 
 # Update shell
 exec zsh
-
-# SSH authentication
-# ssh-keygen -t ed25519 -C "your-email@example.com"
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/id_ed25519
-# ssh-add -l
-# Paste output of cat ~/.ssh/id_ed25519.pub to GitHub ▸ Settings ▸ SSH and GPG keys ▸ New SSH key → paste → Add key.
-# ssh -T git@github.com
-
-# Usage
-# Set remote with dots config --global push.autoSetupRemote true.
-# Update tracked files with dots add -u or Add new files to track with dots add <xyz>.
-# dots commit -m "VERSION_NAME"
-# dots push
-# Update local dotfiles with dots pull --rebase origin master and then dots push if newer changes made to repo from another machine.
