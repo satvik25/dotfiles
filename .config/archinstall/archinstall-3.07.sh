cb4a36f694c2a46f00b8a91acafe0e48749faf6f
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 23:41:34 +0530

    Update and rename archinstall-3.06.sh to archinstall-3.07.sh

--- .config/archinstall/archinstall-3.07.sh
+++ .config/archinstall/archinstall-3.07.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 echo -e "Boot up and run the \033[31mnext [3.0] batch\033[0m of scripts."
-echo -e "\033[32m[CONGRATULATIONS!] ARCH LINUX SET UP!\033[0m"
+echo -e "\033[32m[CONGRATULATIONS!]\033[0m \033[34mARCH LINUX\033[0m \033[35mSET\033[0m \033[36mUP!\033[0m"
