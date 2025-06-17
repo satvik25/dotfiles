7a19c8444672cb028042b77d651ad66d5cb94b09
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 22:56:50 +0530

    Update archinstall-2.0.sh

--- .config/archinstall/archinstall-2.0.sh
+++ .config/archinstall/archinstall-2.0.sh
@@ -7,8 +7,7 @@ curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/a
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.02-bootloader.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.03-secureboot.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.04-tpm.sh" | bash
-curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.05-snapshots.sh" | sudo bash
-curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.06-useradd.sh" | bash
-curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.07-network.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.05-useradd.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.06-network.sh" | bash
 
 echo -e "Boot up and run the \033[31mnext [3.0] batch\033[0m of scripts."
