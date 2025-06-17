f693ddbbbb40666fef5460933e8c6f355751176e
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 19:37:24 +0530

    Update archinstall-1.0.sh

--- .config/archinstall/archinstall-1.0.sh
+++ .config/archinstall/archinstall-1.0.sh
@@ -2,7 +2,11 @@
 set -euo pipefail
 
 # Pre chroot
-# Start with curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.0.sh" | bash
+# Connect to the internet: iwctl station wlan0
+## Troubleshooting tip: Might need to run the following:
+## dhcpcd wlan0
+## ip route add default via 192.168.1.1 dev wlan0
+# Run the scripts: curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.0.sh" | bash
 
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.01-basics.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-1.02-partition.sh" | bash
