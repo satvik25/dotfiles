5bd17c24323c478e42ed6daa06f7c91e83e24317
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 19:40:49 +0530

    Update archinstall-2.0.sh

--- .config/archinstall/archinstall-2.0.sh
+++ .config/archinstall/archinstall-2.0.sh
@@ -7,6 +7,6 @@ curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/a
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.02-bootloader.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.03-secureboot.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.04-tpm.sh" | bash
-curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.05-snapshots.sh" | bash
+curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.05-snapshots.sh" | sudo bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.06-useradd.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-2.07-network.sh" | bash
