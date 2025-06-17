7a1d4cca05d98b5857359732e94b5cc804af8a6a
satvik25 <satvikchaudhary@gmail.com>
2025-06-17 23:44:27 +0530

    Update archinstall-3.0.sh

--- .config/archinstall/archinstall-3.0.sh
+++ .config/archinstall/archinstall-3.0.sh
@@ -9,4 +9,4 @@ curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/a
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-3.04-dotfiles.sh" | bash
 curl -fsSL "https://raw.githubusercontent.com/satvik25/dotfiles/master/.config/archinstall/archinstall-3.05-autostart.sh" | bash
 
-echo -e "\033[32m[CONGRATULATIONS!] ARCH LINUX SET UP!\033[0m"
+echo -e "\033[32m[CONGRATULATIONS!]\033[0m \033[34mArch set up.\033[0m"
