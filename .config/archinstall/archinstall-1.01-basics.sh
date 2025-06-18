126d878c0158251a8aee4419e47ee3e5822db557
satvik25 <satvikchaudhary@gmail.com>
2025-06-18 09:24:33 +0530

    Update archinstall-1.01-basics.sh

--- .config/archinstall/archinstall-1.01-basics.sh
+++ .config/archinstall/archinstall-1.01-basics.sh
@@ -10,19 +10,19 @@ echo -e "\033[35m[4]\033[0m 13.6 pt"
 read -r -p "Choose font size." answer_1 < /dev/tty
 
 case "${answer_1:-2}" in
-    [1])
+    1)
         setfont ter-112n
         echo "S font size set."
         ;;
-    [2])
+    2)
         setfont ter-120n
         echo "M font size set."
         ;;
-    [3])
+    3)
         setfont ter-128n
         echo "L font size set."
         ;;
-    [4])
+    4)
         setfont ter-136n
         echo "XL font size set."
         ;;
@@ -51,43 +51,32 @@ echo -e "\033[35m[2]\033[0m 25%"
 echo -e "\033[35m[3]\033[0m 50%"
 echo -e "\033[35m[4]\033[0m 75%"
 echo -e "\033[35m[4]\033[0m 100%"
-read -r -p "Choose font size." answer_2 < /dev/tty
+read -r -p "Choose brightness level." answer_2 < /dev/tty
 case "${answer_2:-5}" in
-    [1])
-        if echo "$MAX_BRIGHTNESS" | tee "$BACKLIGHT_DIR/brightness" > /dev/null; then
-            echo "Brightness set to 100%."
-        else
-            echo "Failed to set brightness."
-            exit 1
-        fi
+    1)
+        VALUE=0
         ;;
-    [2])
-        if echo "$MAX_BRIGHTNESS" | tee "$BACKLIGHT_DIR/brightness" > /dev/null; then
-            echo "Brightness set to 100%."
-        else
-            echo "Failed to set brightness."
-            exit 1
-        fi
+    2)
+        VALUE=$((MAX_BRIGHTNESS * 25 / 100))
         ;;
-    [3])
-        if echo "$MAX_BRIGHTNESS" | tee "$BACKLIGHT_DIR/brightness" > /dev/null; then
-            echo "Brightness set to 100%."
-        else
-            echo "Failed to set brightness."
-            exit 1
-        fi
+    3)
+        VALUE=$((MAX_BRIGHTNESS * 50 / 100))
         ;;
-    [4])
-        if echo "$MAX_BRIGHTNESS" | tee "$BACKLIGHT_DIR/brightness" > /dev/null; then
-            echo "Brightness set to 100%."
-        else
-            echo "Failed to set brightness."
-            exit 1
-        fi
+    4)
+        VALUE=$((MAX_BRIGHTNESS * 75 / 100))
         ;;
-    *)
+    5)
+        VALUE=$MAX_BRIGHTNESS
+        ;;
+    *) 
         echo -e "\033[31m[Incorrect Input]\033[0m Choose 1, 2, 3, 4 or 5."
         ;;
 esac
+
+if echo "$VALUE" | tee "$BACKLIGHT_DIR/brightness" > /dev/null; then
+    echo "Brightness set to $(( VALUE * 100 / MAX_BRIGHTNESS ))%."
+else
+    echo "Failed to set brightness."
+fi
 
 echo -e "\033[32m[SUCCESS]\033[0m Basics configured."
