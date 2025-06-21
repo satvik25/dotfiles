60da942e32972535339d852b5bbe2dec1bffd33c
7vik <satvikchaudhary@gmail.com>
2025-06-21 16:53:25 +0530

    upd 2025-06-21 16:53:25

---
+++ .local/bin/arch-update.sh
@@ -0,0 +1,16 @@
+#!usr/bin/env bash
+set -euo pipefail
+
+# Arch-update Weekly Timer
+
+systemctl --user disable --now arch-update.timer
+
+mkdir -p ~/.config/systemd/user/arch-update.timer.d
+cat > ~/.config/systemd/user/arch-update.timer.d/override.conf <<EOF
+[Timer]
+OnCalendar=
+OnCalendar=weekly
+EOF
+
+sudo systemctl daemon-reload
+systemctl --user enable --now arch-update.timer
