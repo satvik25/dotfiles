907d4eca5f2b0a8cab43d18e72311a62ab33ba17
7vik <satvikchaudhary@gmail.com>
2025-08-05 00:19:56 +0530

    application category upd

--- .local/bin/device-specific/DELL-Inspiron-13-5378/toggle-onscreen-keyboard.sh
+++ .local/bin/device-specific/DELL-Inspiron-13-5378/toggle-onscreen-keyboard.sh
@@ -6,7 +6,7 @@ DEV_TABLET_STATE="/dev/input/by-path/pci-0000:00:1f.0-platform-INT33D6:00-event"
 OSK_CMD="squeekboard"           # change to wvkbd, maliit‑keyboard, etc.
 # ────────────────────────────────────────────────────────
 
-sleep 30
+sleep 40
 
 # The pipeline stays running forever. `stdbuf -oL` forces
 # each tool to flush on newline so single “0”/“1” tokens
